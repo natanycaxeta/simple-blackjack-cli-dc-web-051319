994d0310059164d1c7c5b5db2047c2224aa161cf
Natany Caxeta <natanycaxeta@icloud.com>
2019-04-22 21:00:11 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -31,7 +31,7 @@ end
 def initial_round
   # code #initial_round here
     first_round = deal_card + deal_card
-  puts display_card_total(first_round)
+  display_card_total(first_round)
   return first_round
 end
 
